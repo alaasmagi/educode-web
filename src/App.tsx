5afd00c9d67cc7864283c7255f385465c4e0fd0e
Aleksander Laasmägi <allaas@taltech.ee>
2025-03-17 13:20:23 +0200

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
-import QrGenerator from "./components/QRGenerator";
+import QrGenerator from "./components/QrGenerator";
 
 function App() {
   return (
     <>
       <QrGenerator value={"000003"} />
+      <h1 className="text-xl text-red-500">Hello tailwind</h1>
     </>
   );
 }
