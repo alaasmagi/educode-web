973f75b14a0fa8d24ebe5272050fe878b0b6a3f7
Aleksander Laasmägi <aleksander.laasmagi@gmail.com>
2025-03-19 08:07:45 +0200

    Changed all messages

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -4,7 +4,7 @@ interface ErrorMessageProperties {
 
 const ErrorMessage: React.FC<ErrorMessageProperties> = ({ text }) => {
   return (
-    <span className="px-5 py-3 bg-[#3F1E20] border-[#DD2D4A] text-[#DD2D4A] text-xl font-semibold border-2 rounded-2xl">
+    <span className="px-5 py-3 bg-[#3F1E20] border-[#DD2D4A] text-[#DD2D4A] text-xl font-semibold border-2 rounded-2xl md:w-xs max-md:w-2xs">
       {text}
     </span>
   );
--- src/components/NormalMessage.tsx
+++ src/components/NormalMessage.tsx
@@ -4,7 +4,7 @@ interface NormalMessageProperties {
 
 const NormalMessage: React.FC<NormalMessageProperties> = ({ text }) => {
   return (
-    <span className="px-5 py-3 bg-[#16325B] border-[#4C97FF] text-[#4C97FF] text-xl font-semibold border-2 rounded-2xl">
+    <span className="px-5 py-3 bg-[#16325B] border-[#4C97FF] text-[#4C97FF] text-xl font-semibold border-2 rounded-2xl md:w-xs max-md:w-2xs">
       {text}
     </span>
   );
