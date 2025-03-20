dda01625ba38bb270afa55af26ff84252cc5f310
Aleksander Laasmägi <aleksander.laasmagi@gmail.com>
2025-03-20 09:33:17 +0200

    Changed courses page structure

--- src/screens/CoursesView.tsx
+++ src/screens/CoursesView.tsx
@@ -1,12 +1,19 @@
 import "../App.css";
+import ContainerCardSmall from "../components/ContainerCardSmall";
 import SideBar from "../layout/SideBar";
 
 function CoursesView() {
   return (
     <>
       <SideBar />
-      <div className="max-h-screen max-w-screen">
-        <h1>Courses</h1>
+      <div className="flex max-h-screen max-w-screen items-center justify-center md:pl-90">
+        {" "}
+        <ContainerCardSmall
+          boldLabelA="Kasutajaliidesed"
+          boldLabelB="(ITI0209)"
+          linkText="View"
+          onClick={console.log}
+        />
       </div>
     </>
   );
